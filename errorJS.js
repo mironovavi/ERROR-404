0d3dc1e6b1ef07eda99740a51df57a3688cc9145
Victoria <v.kurgina@yahoo.com>
2023-03-10 19:50:53 +0300

    added JS code

---
+++ errorJS.js
@@ -0,0 +1,8 @@
+let phrase = "Сервер должен отвечать на входящие HTTP-запросы. Например, когда вы вводите URL в адресную строку браузера, то отправляется запрос на сервер, чтобы тот вернул соответствующую веб-страницу. Если страница была перемещена, удалена или её не существовало вовсе, сервер вернёт ошибку 404.";
+
+let button = document.querySelector(".button");
+let explanation = document.querySelector(".explanation");
+
+button.addEventListener('click', function(){
+    explanation.textContent = phrase;
+});
